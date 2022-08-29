eb973edcfbfc7e32470bff2ad004439afbdc1b17
Mudasir Ahmed <mudasir@mudasirs-mbp.lan>
2022-08-29 14:10:57 -0500

    [Mudasir] initial code commit

---
+++ CopernicanApp/CopernicanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopernicanAppApp.swift
+//  CopernicanApp
+//
+//  Created by Mudasir Ahmed on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CopernicanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
